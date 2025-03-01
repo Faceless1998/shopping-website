0d31bc4575f2960eec3529d7aeb6bdb74429a2f6
Faceless1998 <81959592+Faceless1998@users.noreply.github.com>
2025-03-01 14:28:04 +0400

    new

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://market-server-inky.vercel.app/api';
+const API_URL = 'http://localhost:5000/api';
 
 // Create axios instance
 const api = axios.create({
